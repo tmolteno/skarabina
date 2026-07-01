a92efd24a40afd453d70975aa1f2732bd3a3843b
Tim Molteno <tim@elec.ac.nz>
2026-07-02 08:14:30 +1200

    Add examples directory

---
+++ example/test_run.sh
@@ -0,0 +1,8 @@
+uv run skarabina \
+    --ms /home/tim/astro/merghers/mergA_tim.ms
+    --flag-uv-above 1000 \
+    --flag-spectral-window spectral-flags.example.yml \
+    --optimize \
+    --msout test_spw.ms \
+    --clobber \
+    --summary
