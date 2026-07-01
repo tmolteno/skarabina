639bd2b64ce351f4efa11d95e67a30da7d287398
Tim Molteno <tim@elec.ac.nz>
2026-07-02 11:22:23 +1200

    Update example to do time averaging

--- example/test_run.sh
+++ example/test_run.sh
@@ -2,6 +2,7 @@ uv run skarabina \
     --ms /home/tim/astro/merghers/mergA_tim.ms \
     --flag-uv-above 1000 \
     --flag-spectral-window spectral-flags.example.yml \
+    --time-average-factor 3 \
     --optimize \
     --msout test_spw.ms \
     --clobber \
